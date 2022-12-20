ef7a8858c95fe89fe6e58230e667903a42de846c
Lyubov Trushnikova <lovelly2008@ya.ru>
2022-12-20 20:57:07 +0500

    финиш

--- scripts/index.js
+++ scripts/index.js
@@ -11,11 +11,14 @@ const todos = [
 
   //1.добавляем контейнер куда мы будем добавлять задачи
   const todosConteiner = document.querySelector('.todos__list');
-  
+  const addTodoForm = document.querySelector('.todo-form');
+  const input = addTodoForm.querySelector('.todo-form__input');
+  const template = document.querySelector('#todo-item-template');
 
   //2.создать элемент и добавить на страницу
   //2.1 создаем элемент - функция creatTodo
-  const creatTodo = (taskName) => {
+  //первый способ
+  /*const creatTodo = (taskName) => {
     const li = document.createElement('li');//создали li
     li.classList.add('todo-item');//присвоили ей класс
     const span = document.createElement('span');
@@ -31,20 +34,66 @@ const todos = [
     li.append(span, buttonEdit, buttonCopy, buttonDel);
     // возвращаем тег ли, что бы при вызове функции мы могли получить результата работы
     return li;
-  }
+  }*/
+  //второй способ используем обратные кавычки и вставляем код html. taskName добавляем в span как текст контент и удаление и копирование
+  function creatTodo(taskName) {
+    const task = template.content.querySelector('.todo-item').cloneNode(true);
+    task.querySelector('.todo-item__text').textContent = taskName;
+    task.querySelector('.todo-item__del').addEventListener('click', () => {
+      task.remove();
+    });
+    task.querySelector('.todo-item__copy').addEventListener('click', () => {
+      renderTodo(taskName);
+    });
+    //функция редактирования 
+task.querySelector('.todo-item__edit').addEventListener('click', (e) => {
+//найти элемент текста
+const textElement = task.querySelector('.todo-item__text');
+//делаем этот текстовый элемент редактируемым
+textElement.contentEditable = true;
+//что бы началось редактирование, устанавливаем фокус
+textElement.focus();
+// опишем editTodo
+const editTodo = (e) => {
+  e.preventDefault();
+  textElement.contentEditable = false; 
+  textElement.removeEventListener('blur', editTodo);
+}
+//используем событие blur на этом элементе
+textElement.addEventListener('blur', editTodo);
+
+})
+    return task;
+}
   console.log(creatTodo);
 //2.3 создаем функцию - добавляем в контейнер todosConteiner наш элемент li поэтому передаем в параметры append метод creatTodo
+//для первого способа
+/*
 const renderTodo = (taskName) => {
     todosConteiner.append(creatTodo(taskName))
+}*/
+//для второго. так элементы созданы html кодом
+const renderTodo = (taskName) => {
+  todosConteiner.append(creatTodo(taskName))
 }
-/*
+
 //перебор элементов массива методом forEach
   todos.forEach((title) => {
     renderTodo(title);
   })
-  */
+  
 // 2.4 перебор элементов методом Map и передадим в него нашу функцию колбек с переменной taskName , разворачиваем массив с помощью спред оператора ...
-todosConteiner.append(...todos.map((taskName) => {
+/*todosConteiner.append(...todos.map((taskName) => {
     return creatTodo(taskName);
-}));
+}));*/
 
+// 
+const addTodo = (event) => {
+event.preventDefault();
+const taskName = input.value;
+renderTodo(taskName);
+input.value = '';
+}
+addTodoForm.addEventListener('submit',addTodo);
+
+//делаем delete
